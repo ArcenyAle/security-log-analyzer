24ac2c1f4cecab8001c64f7a22d0be98b3b383c5
ArcenyAle <156490828+ArcenyAle@users.noreply.github.com>
2026-06-01 14:33:56 -0700

    threat identifier

---
+++ threat-detection.py
@@ -0,0 +1,24 @@
+from log_manager import LogRepository
+from analytics import get_log_counts
+from filter import get_logs_by_user
+from datetime import datetime, timedelta
+
+def identify_attack(logger: LogRepository, user: str) -> bool:
+    user_logs = get_logs_by_user(logger, user)
+    failed_logs = [log for log in user_logs if log["event_type"] == "failed_login"]
+    timestamps = sorted([log["timestamp"] for log in failed_logs], reverse=True)
+    datetimes = [datetime.fromisoformat(timestamp) for timestamp in timestamps]
+
+    if len(datetimes) < 5:
+        return False
+    
+    for x in range(1, 5):
+        if (datetimes[0] - datetimes[x]) >= timedelta(minutes=10):
+            return False
+    
+    return True
+
+
+if __name__ == "__main__":
+    logger = LogRepository("logs.json")
+    print(identify_attack(logger, "bob"))
